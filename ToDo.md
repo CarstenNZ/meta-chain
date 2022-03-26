4fb4f60859b490d417f20d7a7b133bd848122b51
CarstenNZ <8205404+CarstenNZ@users.noreply.github.com>
2022-03-27 11:28:06 +1300

    adds Account with xref to references

--- ToDo.md
+++ ToDo.md
@@ -1,3 +1,10 @@
 
-- Account, with 'address'
+- navigation from receipt to block to transaction
+  - add Block.All_Blocks
+  - tests for Accounts and xref 
+
+- text file cache :)
+  - great basis for examples and demo
+
+- Account names
 - implement [Web3 Core Data](https://web3py.readthedocs.io/en/stable/overview.html?highlight=account#api)
--- chainmodel/base.py
+++ chainmodel/base.py
@@ -2,15 +2,11 @@ import base64
 from typing import List, Dict, Set
 
 import yaml
-from hexbytes import HexBytes
 from std_format import Hex
 
 
 class ChainData(yaml.YAMLObject):
     _AttrHandlers = {}
-    _TypeConverter = {
-        HexBytes: lambda hb: Hex.fmt(hb.hex())
-    }
 
     def __init__(self, data_dict):
         """ generic init crates instance attributes from dict items
@@ -24,10 +20,7 @@ class ChainData(yaml.YAMLObject):
     def attr_default_handler(self, key, value):
         """ removes all special types, e.g. HexBytes from web3
         """
-        if not isinstance(value, (int, str, list, type(None))):     # special type
-            value = self._TypeConverter[type(value)](value)
-
-        elif type(value) is str and Hex.isHexStr(value):    # std format for hex strings
+        if type(value) is str and Hex.isHexStr(value):    # std format for hex strings
             value = Hex.fmt(value)
 
         setattr(self, key, value)
@@ -37,11 +30,46 @@ class ChainData(yaml.YAMLObject):
         """
         return yaml.dump(self, indent=4)
 
+    def _ref_account(self, _field, address):
+        Account.add_xref(address, self)
+
     def __str__(self):
         return f"<{self.__class__.__name__}>"
 
 
+class Account(ChainData):
+    All_Accounts: Dict[str, 'Account'] = {}
+
+    def __init__(self, address: str):
+        assert Hex.isHexStr(address)
+
+        super().__init__({})
+        self.address = address
+        self.xref: Set[ChainData] = set()
+
+    @property
+    def name(self):
+        """ short name for account """
+        return base64.b32encode(bytearray.fromhex(self.address[2:12])).decode()
+
+    @staticmethod
+    def add_xref(address: str, ref_data: ChainData):
+        address = Hex.fmt(address)
+        acc = Account.All_Accounts.get(address)
+        if acc is None:
+            Account.All_Accounts[Hex.fmt(address)] = acc = Account(address)
+
+        acc.xref.add(ref_data)
+        return acc
+
+    def __str__(self):
+        return f"<{self.__class__.__name__} {self.address}>"
+
+
 class Transaction(ChainData):
+    _AttrHandlers = {'from': ChainData._ref_account,
+                     'to': ChainData._ref_account}
+
     def __init__(self, data_dict):
         self.hash = ''
         self.blockNumber = -1
@@ -53,6 +81,9 @@ class Transaction(ChainData):
 
 
 class Receipt(ChainData):
+    _AttrHandlers = {'from': ChainData._ref_account,
+                     'to': ChainData._ref_account}
+
     def __init__(self, data_dict):
         self.blockHash = None
         self.transactionHash = None
@@ -75,4 +106,5 @@ class Block(ChainData):
     def __str__(self):
         return f"<{self.__class__.__name__} #{self.number}>"
 
-    _AttrHandlers = {'transactions': _init_transactions}
+    _AttrHandlers = {'transactions': _init_transactions,
+                     'miner': ChainData._ref_account}
